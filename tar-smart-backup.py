86f6a22da9532c52ab8d08f288adbdc1cbb6ae9a
Evgeniy Klemin <evgeniy.klemin@gmail.com>
2016-02-27 05:57:21 +0300

    Add ssh sync feature

--- tar-smart-backup.py
+++ tar-smart-backup.py
@@ -14,8 +14,9 @@ import os
 import errno
 import shutil
 import logging
+import paramiko
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
 TAR_COMMAND_BACKUP = "/bin/tar --file={filename} --listed-incremental={snap} --ignore-failed-read --one-file-system --recursion --preserve-permissions -C {source_dir_parent} -cpz {source_dir_basename}"
@@ -38,18 +39,26 @@ class DefaultHelpParser(argparse.ArgumentParser):
 
 
 def main():
-    """main"""
+    """Main"""
 
     parser = DefaultHelpParser(description='Backup directory.')
     parser.add_argument('name', help='Name of backup')
+    parser.add_argument('--sync', action='store_true',
+                        help='Sync with remote throuh ssh')
+    parser.add_argument('--ssh-key-rsa', help='Private RSA key')
+    parser.add_argument('--ssh-host', help='SSH host')
+    parser.add_argument('--ssh-port', default=22, help='SSH port')
+    parser.add_argument('--ssh-user', default='app', help='SSH port')
+    parser.add_argument('--remote-dir', default='/home/app/backups',
+                        help='Dir in remote backup server')
 
     subparsers = parser.add_subparsers(dest='action', help='Action')
 
     parser_backup = subparsers.add_parser('backup', help='Backup')
     parser_backup.add_argument('src', help='Directory for backup (source)')
-    parser_backup.add_argument('--levels', type=int, default=3,
+    parser_backup.add_argument('--levels', type=int, default=4,
                                help='Max snapshot levels')
-    parser_backup.add_argument('--count', type=int, default=10,
+    parser_backup.add_argument('--count', type=int, default=5,
                                help='Count snapshots on each levels')
     parser_backup.add_argument('--dst', default='.', help='Where hold backups')
 
@@ -68,6 +77,8 @@ def main():
 
 
 def silentremove(filename):
+    """Remove file if exists
+    """
     try:
         os.remove(filename)
     except OSError as e:
@@ -75,71 +86,112 @@ def silentremove(filename):
             raise
 
 
-def backup_level0(args):
+def backup_full(args):
+    """""Full backup
+
+    Returns:
+        int: exit code from tar
+    """
     snap = "{}-snar-0".format(args.name)
-    snap = os.path.join(args.dst, snap)
+    snap_path = os.path.join(args.dst, snap)
     filename = "{}.tar.gz".format(args.name)
-    filename = os.path.join(args.dst, filename)
-    silentremove(snap)
-    silentremove(filename)
+    file_path = os.path.join(args.dst, filename)
+    silentremove(snap_path)
+    silentremove(file_path)
     source_dir_parent = os.path.abspath(os.path.join(args.src, os.pardir))
     source_dir_basename = os.path.basename(args.src)
     command = TAR_COMMAND_BACKUP.format(
-        filename=filename, snap=snap, source_dir_parent=source_dir_parent,
+        filename=file_path, snap=snap_path, source_dir_parent=source_dir_parent,
         source_dir_basename=source_dir_basename
     )
     logger.debug("shell command: {}".format(command))
-    return subprocess.call(command, shell=True)
+    rc = subprocess.call(command, shell=True)
+    return (rc, filename, snap)
 
 
-def backup_levelN(args, levels):
+def backup_incremental(args, levels):
+    """Incremental backup
+
+    Returns:
+        int: exit code from tar
+    """
     level = len(levels)
     num = levels[len(levels) - 1]
     parent_snap = "{}-snar-{}".format(args.name, level - 1)
     parent_snap = os.path.join(args.dst, parent_snap)
     snap = "{}-snar-{}".format(args.name, level)
-    snap = os.path.join(args.dst, snap)
+    snap_path = os.path.join(args.dst, snap)
     old_snap = "{}-snar-{}.old".format(args.name, level)
     old_snap = os.path.join(args.dst, old_snap)
 
-    if not os.path.isfile(snap):
-        shutil.copyfile(parent_snap, snap)
+    if not os.path.isfile(snap_path):
+        shutil.copyfile(parent_snap, snap_path)
     silentremove(old_snap)
-    shutil.copyfile(snap, old_snap)
+    shutil.copyfile(snap_path, old_snap)
 
     filename = args.name
     for num in levels:
         filename += "_{:0>2}".format(num)
     filename += ".tar.gz"
-    filename = os.path.join(args.dst, filename)
+    file_path = os.path.join(args.dst, filename)
 
     source_dir_parent = os.path.abspath(os.path.join(args.src, os.pardir))
     source_dir_basename = os.path.basename(args.src)
     command = TAR_COMMAND_BACKUP.format(
-        filename=filename, snap=snap, source_dir_parent=source_dir_parent,
+        filename=file_path, snap=snap_path, source_dir_parent=source_dir_parent,
         source_dir_basename=source_dir_basename
     )
     logger.debug("shell command: {}".format(command))
-    return subprocess.call(command, shell=True)
+    rc = subprocess.call(command, shell=True)
+    return (rc, filename, snap)
+
+
+def is_snap(name, filename):
+    ln = len(name)
+    return filename[:ln] == name\
+        and filename[ln + 1:ln + 1 + len('-snar-')] == '-snar-'
+
+
+def is_arch(name, filename):
+    ln = len(name)
+    return filename[:len(name)] == name and filename[-len(EXT):] == EXT
 
 
 def find_files(name, destination_dir):
-    """Find snapshot files"""
+    """Find snapshot files
+
+    Returns:
+        list
+    """
     return sorted(
         filename
         for filename in os.listdir(destination_dir)
-        if filename[:len(name)] == name and filename[-len(EXT):] == EXT
+        if is_arch(name, filename)
+    )
+
+
+def find_snap_files(name, destination_dir):
+    """Find snap files
+
+    Returns:
+        list
+    """
+    return sorted(
+        filename
+        for filename in os.listdir(destination_dir)
+        if is_snap(name, filename)
     )
 
 
 def parse_filename(name, filename):
-    """""Parse snapshot filename
+    """Parse snapshot filename
 
-    Example:
-        >>> parse_filename('data', 'data_01_03.tar.gz') -> [(0, 1), (1, 3)]
+    Examples:
+        >>> list(parse_filename('data', 'data_01_03.tar.gz'))
+        [(0, 1), (1, 3)]
 
-    Return:
-        generator(tuple(int, int))
+    Yields:
+        tuple(int, int)
     """
     item = filename[len(name) + 1:-len(EXT)]
     parts = item.split('_')
@@ -153,9 +205,9 @@ def parse_filename(name, filename):
 
 
 def scan_dir(name, destination_dir):
-    """""Return list of levels with max num
+    """Return list of levels with max num
 
-    Example:
+    Examples:
 
         destination_dir content:
 
@@ -165,12 +217,13 @@ def scan_dir(name, destination_dir):
             data_01_02.tar.gz
             data_01_03.tar.gz
 
-        >>> scan_dir('data', destination_dir) -> [1, 3]
+        >>> scan_dir('data', destination_dir)
+        [1, 3]
 
         1: LEVEL-1 depth=1 num=1
         3: LEVEL-1 depth=2 num=3
 
-    Return:
+    Returns:
         list
     """
     found = find_files(name, destination_dir)
@@ -186,22 +239,28 @@ def scan_dir(name, destination_dir):
     return res
 
 
-def clear_old(name, destination_dir, levels):
-    """Clear old level snapshots"""
+def find_files_for_delete(name, destination_dir, levels):
+    """Clear old level snapshots
+
+    Returns:
+        list: List of old files for delete
+    """
     found = find_files(name, destination_dir)
-    for_delete = []
+    res = []
     for filename in found:
         for part_index, value in parse_filename(name, filename):
             if part_index > len(levels) - 2:
-                for_delete.append(filename)
-    for filename in for_delete:
-        os.remove(os.path.join(destination_dir, filename))
+                res.append(filename)
+    return res
 
 
 def backup(args):
+    """Backup directory
+    """
     levels = scan_dir(args.name, args.dst)
     create_full = False
     new_levels = list(levels) if levels else []
+    old_files_for_delete = []
 
     if levels is None:
         create_full = True
@@ -215,7 +274,9 @@ def backup(args):
             else:
                 last_num = new_levels[len(new_levels) - 1]
                 while last_num > args.count - 1:
-                    clear_old(args.name, args.dst, new_levels)
+                    old_files_for_delete += find_files_for_delete(args.name,
+                                                                  args.dst,
+                                                                  new_levels)
                     new_levels = new_levels[:-1]
                     if new_levels:
                         last_num = new_levels[len(new_levels) - 1]
@@ -227,16 +288,33 @@ def backup(args):
 
     if create_full:
         logger.info("Create full backup, args: {}".format(args))
-        rc = backup_level0(args)
+        rc, filename, snap = backup_full(args)
     else:
         logger.info("Create incremental backup, args: {}".format(args))
-        rc = backup_levelN(args, new_levels)
-    if rc != 0:
-        logger.error("Backup with args: {} error".format(args.dst))
+        rc, filename, snap = backup_incremental(args, new_levels)
+
+    if rc == 0:
+        if args.sync:
+            upload_file(filename, args)
+            upload_file(snap, args)
+        for filename in old_files_for_delete:
+            os.remove(os.path.join(args.dst, filename))
+        if args.sync and old_files_for_delete:
+            remote_delete(old_files_for_delete, args)
+        logger.info("Backup {} successed".format(args.name))
+        if args.sync:
+            sync_remote(args)
+    else:
+        logger.error("Backup with args: {} error".format(args))
+
     return rc
 
 
 def restore(args):
+    """Restore backup to directory
+    """
+    if args.sync:
+        download_files(args)
     found = find_files(args.name, args.src)
     if not os.path.exists(args.dst):
         os.makedirs(args.dst)
@@ -249,15 +327,98 @@ def restore(args):
         command = TAR_COMMAND_RESTORE.format(
             filename=file_path, destination_dir=args.dst, snap=snap
         )
-        logger.debug("shell command: {}".format(command))
+        logger.debug("Shell command: {}".format(command))
         rc = subprocess.call(command, shell=True)
         if rc != 0:
-            logger.error("Restore from {}, {} to {} error".format(file_path,
-                                                                  snap,
-                                                                  args.dst))
+            logger.error("Restore with args: {} error".format(args))
             return rc
-    logger.info("Restore backup in {} successed".format(args.dst))
+    logger.info("Restore {} successed".format(args.name))
     return 0
+
+
+def get_ssh_client(args):
+    """Get ssh client
+    """
+    c = paramiko.SSHClient()
+    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    key = None
+    if args.ssh_key_rsa:
+        key = paramiko.RSAKey.from_private_key_file(args.ssh_key_rsa)
+    c.connect(hostname=args.ssh_host, username=args.ssh_user,
+              port=args.ssh_port, pkey=key)
+    return c
+
+
+def upload_file(filename, args):
+    """Upload new file to remote server through sftp
+    """
+    with get_ssh_client(args) as c:
+        with c.open_sftp() as sftp:
+            localpath = os.path.join(args.dst, filename)
+            remotepath = os.path.join(args.remote_dir, filename)
+            sftp.put(localpath, remotepath)
+    logger.info("Upload file {} successed".format(filename))
+
+
+def remote_delete(files, args):
+    """Delete files on remote server
+    """
+    with get_ssh_client(args) as c:
+        files_for_args = ' '.join([
+            os.path.join(args.remote_dir, filename)
+            for filename in files
+        ])
+        command = "rm -f {}".format(files_for_args)
+        stdin, stdout, stderr = c.exec_command(command)
+        stdin.close()
+    logger.info("Delete remote files {} successed".format(files))
+
+
+def remote_find_files(client, args):
+    name = args.name
+    command = "ls -1 {}".format(args.remote_dir)
+    logger.debug("SSH command: {}".format(command))
+    stdin, stdout, stderr = client.exec_command(command)
+    stdin.close()
+    res = stdout.read()
+    logger.debug("SSH result: {}".format(res.splitlines()))
+    files = res.splitlines()
+    arch_files = sorted(
+        filename for filename in files if is_arch(name, filename)
+    )
+    snap_files = sorted(
+        filename for filename in files if is_snap(name, filename)
+    )
+    return arch_files + snap_files
+
+
+def download_files(args):
+    """Download backup files from remote server through sftp
+    """
+    name = args.name
+    with get_ssh_client(args) as c:
+        found = remote_find_files(c, args)
+        with c.open_sftp() as sftp:
+            for filename in found:
+                remotepath = os.path.join(args.remote_dir, filename)
+                localpath = os.path.join(args.src, filename)
+                silentremove(localpath)
+                sftp.get(remotepath, localpath)
+    logger.info("Download backup files successed")
+
+
+def sync_remote(args):
+    """Compare files in local and remote, upload if not found
+    """
+    with get_ssh_client(args) as c:
+        remote_found = remote_find_files(c, args)
+    local_found = find_files(args.name, args.dst)
+    local_snap_found = find_snap_files(args.name, args.dst)
+
+    for local_filename in local_found + local_snap_found:
+        if local_filename not in remote_found:
+            logger.info("Upload missing file {}".format(local_filename))
+            upload_file(local_filename, args)
 
 
 if __name__ == '__main__':
